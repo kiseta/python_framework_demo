05ee6f78a62ed8116e00fb7e6ef618b3d3b7bde7
Tatyana Kiseleva <kiseta@gmail.com>
2020-10-10 20:52:38 -0700

    Removed implicit wait

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,6 @@ __author__ = 'tk'
 class Application:
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
