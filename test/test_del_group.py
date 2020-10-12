c8c3bfc85ab0927c098b74961cb50915bae16957
Tatyana Kiseleva <kiseta@gmail.com>
2020-10-12 14:52:11 -0700

    refactored to delete and mofidy random group

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,17 +1,21 @@
 __author__ = 'tk'
+from model.group import Group
+from random import randrange
 
 
-def test_delete_first_group(app):
+def test_delete_random_group(app):
     app.group.check_group_present("Test Name","Test Header", "Test Footer")
 
     old_groups = app.group.get_group_list()
-    print("Original Groups Count: " + str(len(old_groups)))
 
-    app.group.delete_first_group()
+    index = randrange(len(old_groups))
+    print("Original Groups Count: " + str(len(old_groups)))
+    print("Random Index: " + str(index))
+    app.group.delete_group_by_index(index)
 
     new_groups = app.group.get_group_list()
     print("New Groups Count: " + str(len(new_groups)))
     assert len(old_groups) - 1 == len(new_groups)
-    old_groups[0:1] = []
+    old_groups[index:index+1] = []
     assert old_groups == new_groups
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,7 @@
 __author__ = 'tk'
 
 from model.group import Group
+from random import randrange
 
 
 def test_modify_first_group_name(app):
@@ -8,14 +9,14 @@ def test_modify_first_group_name(app):
 
     old_groups = app.group.get_group_list()
     print("Original Groups Count: " + str(len(old_groups)))
-
+    index = randrange(len(old_groups))
     group = Group(name="Modified Group Name")
-    app.group.modify_first_group(group)
-    group.id = old_groups[0].id
+    app.group.modify_group_by_index(index, group)
+    group.id = old_groups[index].id
     new_groups = app.group.get_group_list()
     print("New Groups Count: " + str(len(new_groups)))
     assert len(old_groups)  == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 # def test_modify_first_group_header(app):
