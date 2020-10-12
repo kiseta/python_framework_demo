9ee7f45960a0e1dfeccaa58d15d9445831c8ac74
Tatyana Kiseleva <kiseta@gmail.com>
2020-10-12 13:02:58 -0700

    added group count hashing in add group test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,28 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import random
 
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
     print("Original Groups Count: " + str(len(old_groups)))
-    s = str(random.randint(111, 999))
-    group = Group(name="grname" + s, header="grheader" + s, footer="grfooter" + s)
+    group = Group(name="asd234", header="asd35", footer="asd234")
     app.group.create(group)
+    print("New Groups Count: " + str(app.group.count()))
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    print("New Groups Count: " + str(len(new_groups)))
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    print("Original Groups Count: " + str(len(old_groups)))
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    print("New Groups Count: " + str(len(new_groups)))
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_add_empty_group(app):
+#     old_groups = app.group.get_group_list()
+#     print("Original Groups Count: " + str(len(old_groups)))
+#     group = Group(name="", header="", footer="")
+#     app.group.create(group)
+#     new_groups = app.group.get_group_list()
+#     print("New Groups Count: " + str(len(new_groups)))
+#     assert len(old_groups) + 1 == len(new_groups)
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
