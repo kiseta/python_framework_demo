356660f46b6aa92e3cdc4e9cbe01029b2866b485
Tatyana Kiseleva <kiseta@gmail.com>
2020-10-14 21:37:00 -0700

    added test and validation for phone numbers on view page (regex)

--- fixture/contact.py
+++ fixture/contact.py
@@ -63,6 +63,17 @@ class ContactHelper:
                        homephone=homephone, mobilephone=mobilephone,
                        workphone=workphone, secondaryphone=secondaryphone)
 
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(homephone=homephone, mobilephone=mobilephone,
+                       workphone=workphone, secondaryphone=secondaryphone)
+
     def create(self, contact):
         wd = self.app.wd
         self.open_add_new_page()
