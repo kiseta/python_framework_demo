86a07225e759922299aac7c3dcf3f1b785448d5a
Tatyana Kiseleva <kiseta@gmail.com>
2020-10-17 21:46:46 -0700

    updated finding config file relative path to project folder vs working folder

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ __author__ = 'tk'
 
 import pytest
 import json
+import os.path
 from fixture.application import Application
 
 fixture = None
@@ -13,9 +14,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target= json.load(config_file)
-
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target= json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target['baseUrl'])
     fixture.session.ensure_login(username=target['username'], password=target['password'])
