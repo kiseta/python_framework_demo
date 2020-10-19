2528b791a860888282acd42c44f32ac80cf80038
Tatyana Kiseleva <kiseta@gmail.com>
2020-10-18 22:22:30 -0700

    replaces testdata parameter with fakedata parameter

--- data/groups.py
+++ data/groups.py
@@ -5,12 +5,12 @@ from faker import Faker
 
 fake = Faker('en_US')
 
-testdata = [
-    Group(name="name1", header="header1", footer="footer1"),
-    Group(name="name2", header="header2", footer="footer2")
-]
+# testdata = [
+#     Group(name="name1", header="header1", footer="footer1"),
+#     Group(name="name2", header="header2", footer="footer2")
+# ]
 
-fakedata = [
+testdata = [
     Group(name=fake.pystr_format(), header=fake.pystr(), footer=fake.pystr_format()),
     Group(name=fake.pystr(), header=fake.pystr_format(), footer=fake.pystr())
 ]
