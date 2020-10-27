fb0b58a19de4a5a49bff3d4d10b9269b414fb300
Tatyana Kiseleva <kiseta@gmail.com>
2020-10-26 19:39:42 -0700

    implemented db connection fixture

---
+++ fixture/db.py
@@ -0,0 +1,18 @@
+__auth__ = 'tk'
+
+import mysql.connector
+from model.group import Group
+from model.contact import Contact
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+        self.connection.autocommit = True
+
+    def destroy(self):
+        self.connection.close()
