9f41ba73b73b461bc9ee2ee2019d73bdcbedb20a
Tatyana Kiseleva <kiseta@gmail.com>
2020-10-30 01:32:43 -0700

    Implemented get db contacts method

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,13 +1,35 @@
 __auth__ = 'tk'
 
-import mysql.connector
+# import mysql.connector
+#
+# connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+#
+# try:
+#     cursor = connection.cursor()
+#     cursor.execute("select * from group_list")
+#     for row in cursor.fetchall():
+#         print(row)
+# finally:
+#     connection.close()
 
-connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+from fixture.db import DbFixture
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
 finally:
-    connection.close()
+    db.destroy()
+
+
+try:
+    groups = db.get_group_list()
+    for group in groups:
+        print(group)
+    print(len(groups))
+finally:
+    db.destroy()
+
--- fixture/db.py
+++ fixture/db.py
@@ -26,5 +26,17 @@ class DbFixture:
             cursor.close()
         return list
 
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+        finally:
+            cursor.close()
+        return list
+
     def destroy(self):
         self.connection.close()
