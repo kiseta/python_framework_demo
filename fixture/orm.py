09bec3e8dc9df529c55deb675fdb700ad7ed39fb
Tatyana Kiseleva <kiseta@gmail.com>
2020-11-08 12:48:22 -0800

    Added db connection

---
+++ fixture/orm.py
@@ -0,0 +1,26 @@
+__author__ = 'tk'
+
+from pony.orm import *
+from model.group import Group
+from model.contact import Contact
+from datetime import datetime
+from pymysql.converters import decoders
+
+
+class ORMFixture:
+
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        id = PrimaryKey(int, column='group_id')
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        firstname = Optional(str, column='firstname')
+        lastname = Optional(str, column='lastname')
+        deprecated = Optional(datetime, column='deprecated')
